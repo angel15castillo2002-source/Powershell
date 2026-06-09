861db6767246e0c76c2e8e7eab24137a79fdb9a2
angel15castillo2002-source <angel15castillo2002@gmail.com>
2026-06-09 11:18:56 -0400

    importingusers

--- event.ps1
+++ event.ps1
@@ -1,2 +1,6 @@
 Get-EventLog -LogName Application -Newest 10
 winget install --id Microsoft.PowerShell --source winget
+Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser
+Install-Module -Name PSReadLine -Force -AllowClobber -Scope CurrentUser
+Install-Module -Name PSScriptAnalyzer -Force -AllowClobber -Scope CurrentUser
+Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser
