8d4599ba9172d1ce8841bc9bcc33694e193a54cb
angel15castillo2002-source <angel15castillo2002@gmail.com>
2026-06-11 09:37:39 -0400

    update

---
+++ Hello World.ps1
@@ -0,0 +1,7 @@
+# Script 1: Hello World and user greeting
+
+$name = Read-Host "Enter your name"
+
+Write-Host "Hello, $name! Welcome to PowerShell."
+Write-Host "Today is: " (Get-Date)
+Write-Host "Have a great day!"
